fd53903ecf7eed3c9a90a785a7a6700bd8d6e758
Jakub Cebula <kubcor1@gmail.com>
2021-04-30 12:11:13 +0200

    PR_L7_2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route, Switch} from "react-router-dom";
+import {Redirect} from 'react-router';
 import Posts from './components/posts';
+import Home from "./components/home";
+import NavBar from "./components/navbar";
+import NotFound from "./components/notFound";
 
 function App() {
-  return (
-    <Posts />
-  );
+    return (
+        <div className="container-fluid">
+            <NavBar/>
+            <div className="container">
+                <div className="content">
+                    <Switch>
+                        <Route path="/posts"
+                               component={Posts}/>}
+                        />
+                        <Route path="/" exact component={Home}/>
+                        <Route path="/not-found" exact component={NotFound}/>
+                        <Redirect to="/not-found"/>
+                    </Switch>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
